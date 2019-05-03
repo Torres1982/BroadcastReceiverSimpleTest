5d8be64f18c8b81817f374fd6ea8bce26ad83c0b
Artur Torres <artur.sukiennik82@gmail.com>
2019-05-03 10:04:06 +0100

    Broadcast Receiver finally works (registration and receiving Broadcast successful)

--- app/src/main/java/com/wipro/wiprobroadcastreceiversms/MainActivity.java
+++ app/src/main/java/com/wipro/wiprobroadcastreceiversms/MainActivity.java
@@ -1,6 +1,7 @@
 package com.wipro.wiprobroadcastreceiversms;
 
 import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,14 +28,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // Send Registered Broadcast
+    // Register Broadcast Receiver
     private void broadcastMessage() {
-        Toast.makeText(this, "Broadcast Receiver SENT successfully!", Toast.LENGTH_SHORT).show();
-
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("com.wipro.wiprobroadcastreceiversms.testbroadcast");
         broadcastReceiver = new MyTestReceiver();
         registerReceiver(broadcastReceiver, intentFilter);
+
+        Toast.makeText(this, "Broadcast Receiver REGISTERED successfully!", Toast.LENGTH_SHORT).show();
+        sendBroadcast();
+    }
+
+    // Send Broadcast
+    private void sendBroadcast() {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        intent.setAction("com.wipro.wiprobroadcastreceiversms.testbroadcast");
+        sendBroadcast(intent);
+
+        Toast.makeText(this, "Broadcast Receiver SENT successfully!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/wipro/wiprobroadcastreceiversms/MyTestReceiver.java
+++ app/src/main/java/com/wipro/wiprobroadcastreceiversms/MyTestReceiver.java
@@ -6,12 +6,11 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class MyTestReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Toast.makeText(context, "Broadcast Receiver RECEIVED successfully!", Toast.LENGTH_SHORT).show();
 //        if (intent.getAction().equals("com.wipro.wiprobroadcastreceiversms.testbroadcast")) {
-            Toast.makeText(context, "Broadcast Receiver RECEIVED successfully!", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(context, "Broadcast Receiver RECEIVED successfully!", Toast.LENGTH_SHORT).show();
 //        }
     }
 }
