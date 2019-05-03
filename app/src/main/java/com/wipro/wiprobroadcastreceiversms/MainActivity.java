9228efe954b5027adc4bfe10bf74605b01ad8567
Artur Torres <artur.sukiennik82@gmail.com>
2019-05-03 09:45:27 +0100

    Updated and cleaned

--- app/src/main/java/com/wipro/wiprobroadcastreceiversms/MainActivity.java
+++ app/src/main/java/com/wipro/wiprobroadcastreceiversms/MainActivity.java
@@ -1,13 +1,11 @@
 package com.wipro.wiprobroadcastreceiversms;
 
 import android.content.BroadcastReceiver;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sendMessageButton = findViewById(R.id.button_send_sms);
+        sendMessageButton = findViewById(R.id.button_send_broadcast);
 
         sendMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     // Send Registered Broadcast
     private void broadcastMessage() {
         Toast.makeText(this, "Broadcast Receiver SENT successfully!", Toast.LENGTH_SHORT).show();
+
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("com.wipro.wiprobroadcastreceiversms.testbroadcast");
         broadcastReceiver = new MyTestReceiver();
